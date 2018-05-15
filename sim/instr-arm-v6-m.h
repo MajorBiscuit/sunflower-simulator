29c7374331b4c5164c9a6d13cf6635078b512b2c
Andreas Theodosiou <andreas@atheod.net>
2018-05-15 14:24:27 +0100

    Fixes whitespace typo

--- sim/instr-arm-v6-m.h
+++ sim/instr-arm-v6-m.h
@@ -8,18 +8,18 @@
 	are met:
 
 	*	Redistributions of source code must retain the above
-		copyright notice, this list of conditions and the following
-		disclaimer.
+  copyright notice, this list of conditions and the following
+  disclaimer.
 
 	*	Redistributions in binary form must reproduce the above
-		copyright notice, this list of conditions and the following
-		disclaimer in the documentation and/or other materials
-		provided with the distribution.
+  copyright notice, this list of conditions and the following
+  disclaimer in the documentation and/or other materials
+  provided with the distribution.
 
 	*	Neither the name of the author nor the names of its
-		contributors may be used to endorse or promote products
-		derived from this software without specific prior written
-		permission.
+  contributors may be used to endorse or promote products
+  derived from this software without specific prior written
+  permission.
 
 	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 	"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
@@ -37,17 +37,17 @@
 
 enum
   {
-  INSTR_ADR,
-  INSTR_ADDSPI,
-  INSTR_BL_HI,
-  INSTR_BL_LO,
-	INSTR_SHIFT,
-	INSTR_DATA,
-	INSTR_SPECIAL_DATA,
-	INSTR_LOAD_STORE,
-	INSTR_MISC,
-	INSTR_CBRANCH,
-	INSTR_UBRANCH,
-  INSTR_LDR,
-  INSRR_LSMP,
-};
+    INSTR_ADR,
+    INSTR_ADDSPI,
+    INSTR_BL_HI,
+    INSTR_BL_LO,
+    INSTR_SHIFT,
+    INSTR_DATA,
+    INSTR_SPECIAL_DATA,
+    INSTR_LOAD_STORE,
+    INSTR_MISC,
+    INSTR_CBRANCH,
+    INSTR_UBRANCH,
+    INSTR_LDR,
+    INSRR_LSMP,
+  };
